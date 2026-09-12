229d4ad624244384f6f998e62ed11ebd7345e5d5
p3arldiv3r <lilybtrent@gmail.com>
2026-09-12 12:54:09 -0400

    v1

---
+++ cart/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Item, Order
+admin.site.register(Order)
+admin.site.register(Item)
+# Register your models here.
---
+++ moviereviews/settings.py
@@ -0,0 +1,9 @@
+from moviesstore.settings import BASE_DIR
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
