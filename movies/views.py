da447ba82aa357bb348d35813541fc8953d8c1bd
p3arldiv3r <lilybtrent@gmail.com>
2026-09-12 20:45:12 -0400

    last user story flag update

--- movies/views.py
+++ movies/views.py
@@ -61,3 +61,4 @@ def delete_review(request, id, review_id):
     review = get_object_or_404(Review, id=review_id, user=request.user)
     review.delete()
     return redirect('movies.show', id=id)
+
